64f23b6740a7b00201d66834810101712022a60e
evening <39542044+evening@users.noreply.github.com>
2019-12-30 17:04:32 -0500

    base

--- game.py
+++ game.py
@@ -1,20 +1,15 @@
-# Game Object
 import re
 from enum import Enum
 from pathlib import Path
 from datetime import datetime, timedelta
 
+
 class GameMode(Enum):
     MARATHON = 1
     LINE_RACE = 2
 
-class Game:
-    timestamp = datetime.min
-    mode = 0
-    score = 0
-    time = timedelta()
-    directory = Path()
 
+class Game(object):
     def __init__(self, replay_dir):
         self.directory = Path(replay_dir)
         if self.directory.suffix != ".rep":
@@ -22,14 +17,22 @@ class Game:
 
         replay_data = self.directory.read_text()
 
-        timestamp_raw_date = re.findall(r'timestamp\.date=(.+)', replay_data)[0]
-        timestamp_raw_time = re.findall(r'timestamp\.time=(.+)', replay_data)[0].replace("\\","")
-        timestamp_date = datetime.strptime(timestamp_raw_date, '%Y/%m/%d')
-        timestamp_time = datetime.strptime(timestamp_raw_time,"%H:%M:%S").time()
-        self.timestamp = datetime.combine(timestamp_date,timestamp_time)
+        timestamp_raw_date = re.findall(r"timestamp\.date=(.+)", replay_data)[0]
+        timestamp_raw_time = re.findall(r"timestamp\.time=(.+)", replay_data)[
+            0
+        ].replace("\\", "")
+        timestamp_date = datetime.strptime(timestamp_raw_date, "%Y/%m/%d")
+        timestamp_time = datetime.strptime(timestamp_raw_time, "%H:%M:%S").time()
+        self.timestamp = datetime.combine(timestamp_date, timestamp_time)
 
-        self.score = re.findall(r'statistics\.score=(.+)', replay_data)[0]
-        self.time = timedelta(seconds=int(re.findall(r'result\.time=(\d+)', replay_data)[0]))
+        self.score = re.findall(r"statistics\.score=(.+)", replay_data)[0]
+        self.time = timedelta(
+            seconds=int(re.findall(r"result\.time=(\d+)", replay_data)[0])
+        )
 
-        mode = re.findall(r'name\.mode=(.+)', replay_data)[0].replace(" ","_")
+        mode = re.findall(r"name\.mode=(.+)", replay_data)[0].replace(" ", "_")
         self.mode = getattr(GameMode, mode)
+
+
+class Replays(object):
+    pass
--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
 # nullpomino stats
+
+usage:
+```
+g = Game("C:\\NullpoMino\\replay\\2019_12_20_20_09_39.rep")
+g.score
+```
